f22d69d251d38bb289840da911ae693109d08efb
David Ryan <davidryan1998@hotmail.com>
2018-05-24 14:04:40 +0700

    Logarithmic interpolation methods added to interpMethods

--- audio/interpMethods.m
+++ audio/interpMethods.m
@@ -31,10 +31,25 @@ function [retval] = interpMethods (numberOfPoints,startValue,endValue,methodType
 % 3 - cubic interpolation with turning points at start and end
 % 4 (or -2) - quadratic interpolation with turning point at end
 
+% 11 - linear interpolation of logarithms (then scale back up)
+% 12 - quad
+% 13 - cubic
+% 14 - quad backwards
+
 % numberOfPoints checked on inner script
 start1 = startValue(1);
 end1 = endValue(1);
 method1 = real(methodType(1));
+
+if method1 >= 10
+  % Call function with different parameters to get the logarithmic version
+  logStartValue = log(max(1e-6, abs(startValue)));
+  logEndValue = log(max(1e-6, abs(endValue)));
+  % (Start and end values ought to be positive for logarithmic,
+  % but do this checking just in case)
+  retval = exp(interpMethods(numberOfPoints,logStartValue,logEndValue,methodType-10));
+  return;
+endif
 
 diff1 = end1-start1;
 
